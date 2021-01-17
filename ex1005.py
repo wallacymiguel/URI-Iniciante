dddaf1214f9f5ea835ec3811389345354edde816
Wallacy Miguel <wallacycunha@outlook.com>
2021-01-17 14:49:37 -0300

    Novos exercicios

--- ex1005.py
+++ ex1005.py
@@ -1,4 +1,6 @@
-'''Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.'''
+"""Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir,
+calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11).
+Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal. """
 A = float(input())
 B = float(input())
 MEDIA = (A * 3.5 + B * 7.5)/11
